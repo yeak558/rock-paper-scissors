57e2ca0f898ee27d3cbe83b2d12623861df2df78
yeak558 <yeak558@gmail.com>
2026-02-03 12:58:40 +0300

    Add random choice functionality

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+function getComputerChoice() {
+	let randomValue = Math.floor(Math.random() * 3) + 1;
+
+	if (randomValue === 1) { return "Rock"; }
+	else if (randomValue === 2) { return "Paper"; }
+	else if (randomValue === 3) { return "Scissors"; }
+}
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
